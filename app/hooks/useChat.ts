7d435b5f1003c39ccd517ca5e4055b625d850cb5
twopats <lpatipat@icloud.com>
2023-07-31 16:15:40 -0400

    improved dev comments

--- app/hooks/useChat.ts
+++ app/hooks/useChat.ts
@@ -1,11 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useRef, useState } from "react";
 import { Message } from "../types/types";
-import axios, { Axios } from "axios";
+import axios from "axios";
 
 const INTIAL_MESSAGES: Message[] = [];
-const INTIAL_INPUT: string = "";
 
-const testEndPoint = "https://dog-api.kinduff.com/api/facts?number=5";
+const testEndPoint = "http://localhost:8080/api";
 const textEndPoint =
   "https://2dbf-2001-569-7e44-1200-30ac-638a-ca29-1c6e.ngrok-free.app/text";
 
@@ -13,13 +12,7 @@ export default function useChat() {
   const [messages, setMessages] = useState(INTIAL_MESSAGES);
   const inputRef = useRef<HTMLInputElement>(null);
 
-  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
-    e.preventDefault();
-    sendUserInput();
-    postCompletion();
-    clearInput();
-  }
-
+  //add user input to messages array state
   function sendUserInput() {
     if (!inputRef.current) return;
     setMessages([
@@ -28,16 +21,17 @@ export default function useChat() {
     ]);
   }
 
+  //clear chat input field
   function clearInput() {
     if (!inputRef.current) return;
     inputRef.current.value = "";
   }
+
+  //send request to backend server
   async function postCompletion() {
     console.log("postCompletion");
     try {
-      const res = await axios.get(
-        "https://2dbf-2001-569-7e44-1200-30ac-638a-ca29-1c6e.ngrok-free.app/chat?content=hello&role=user"
-      );
+      const res = await axios.get(testEndPoint);
       setMessages((prevState) => {
         return [...prevState, { content: res.data.message, role: "assistant" }];
       });
@@ -46,9 +40,18 @@ export default function useChat() {
     }
   }
 
+  //event handler for chat input
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
     if (!inputRef.current) return;
     inputRef.current.value = e.target.value;
+  }
+
+  //event handler for input form
+  function handleFormSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    sendUserInput();
+    postCompletion();
+    clearInput();
   }
 
   return {
