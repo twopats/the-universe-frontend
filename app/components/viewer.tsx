0c8a4f6e52fe26e7aa0beaa45bd5dbd37a7d6384
twopats <lpatipat@icloud.com>
2023-08-01 04:18:54 -0400

    fix react errors

--- app/components/viewer.tsx
+++ app/components/viewer.tsx
@@ -1,45 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import Image from 'next/image';
+import React, { useState, useEffect } from "react";
+import config from "../config/config";
+import axios from "axios";
+import Image from "next/image";
+import defaultBG from "../public/default-bg.png";
 
-const imageUrls = [
-    '/image1.png',
-    '/image2.png',
-    '/image3.png',
-    // add more image paths here
-];
+const { getImageEndPoint } = config;
+
+const imageEndPoint = getImageEndPoint();
+
+// const imageUrls = [
+//   "/image1.png",
+//   "/image2.png",
+//   "/image3.png",
+//   // add more image paths here
+// ];
 
 function Viewer() {
-    const [currentImage, setCurrentImage] = useState(imageUrls[0]);
+  const [currentImage, setCurrentImage] = useState<string>();
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCurrentImage((prevImage) => {
-                const currentIndex = imageUrls.indexOf(prevImage);
-                if (currentIndex === imageUrls.length - 1) {
-                    // if it was the last image, return the first one
-                    return imageUrls[0];
-                } else {
-                    // otherwise, return the next one
-                    return imageUrls[currentIndex + 1];
-                }
-            });
-        }, 10000); // change image every 10 seconds
+  useEffect(() => {
+    axios
+      .get(imageEndPoint)
+      .then((response) => {
+        if (response) {
+          setCurrentImage(response.data);
+          console.log(response.data);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
 
-        return () => clearInterval(interval);
-    }, []);
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrentImage((prevImage) => {
+  //       const currentIndex = imageUrls.indexOf(prevImage);
+  //       if (currentIndex === imageUrls.length - 1) {
+  //         // if it was the last image, return the first one
+  //         return imageUrls[0];
+  //       } else {
+  //         // otherwise, return the next one
+  //         return imageUrls[currentIndex + 1];
+  //       }
+  //     });
+  //   }, 10000); // change image every 10 seconds
 
-    return (
-        <div className="w-full relative bg-cover overflow-hidden">
-            <Image
-                src={currentImage}
-                layout="fill" // This should be 'layout', not 'fill'
-                objectFit="cover" // 'objectFit' is a prop, not a style
-                quality={100} // you can adjust this for better image quality
-                alt="[viewer picture]"
-            />
-        </div>
-    );
+  //   return () => clearInterval(interval);
+  // }, []);
+
+  return (
+    <div className="w-full relative bg-cover overflow-hidden">
+      <Image
+        src={currentImage || defaultBG}
+        fill
+        style={{ objectFit: "cover" }}
+        quality={100} // you can adjust this for better image quality
+        alt="[viewer picture]"
+      />
+    </div>
+  );
 }
 
 export default Viewer;
-
--- app/page.tsx
+++ app/page.tsx
@@ -6,58 +6,60 @@ import AIChatMessage from "./components/ai-chat-message";
 import useCustomChat from "./hooks/useChat";
 
 export default function Chat() {
-    const audioRef = useRef<HTMLAudioElement | null>(null);
-    const [isFirstMessage, setIsFirstMessage] = useState(true);
+  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [isFirstMessage, setIsFirstMessage] = useState(true);
 
-    const {
-        messages,
-        postCompletion,
-        inputRef,
-        handleInputChange,
-        handleFormSubmit: originalHandleFormSubmit,
-    } = useCustomChat();
+  const {
+    messages,
+    postCompletion,
+    inputRef,
+    handleInputChange,
+    handleFormSubmit: originalHandleFormSubmit,
+  } = useCustomChat();
 
-    useEffect(() => {
-        audioRef.current = new Audio("/audiocraft.mp3");
-    }, []);
+  useEffect(() => {
+    audioRef.current = new Audio("/audiocraft.mp3");
+  }, []);
 
-    const handleFormSubmit = async (e) => {
-        e.preventDefault();
+  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-        if (isFirstMessage) {
-            setIsFirstMessage(false);
-            if (audioRef.current) {
-                await audioRef.current.play();
-            }
-        }
+    if (isFirstMessage) {
+      setIsFirstMessage(false);
+      if (audioRef.current) {
+        await audioRef.current.play();
+      }
+    }
 
-        originalHandleFormSubmit(e);
-    };
+    originalHandleFormSubmit(e);
+  };
 
-    return (
-        <div className="flex  h-screen">
-            <div className=" w-full p-4   max-w-xl flex flex-col stretch   overflow-y-auto divide-y-2 divide-dashed divide-slate-700">
-                {messages.length > 0
-                    ? messages.map((m) => {
-                        if (m.role === "user") {
-                            return <UserChatMessage key={m.id} text={m.content}></UserChatMessage>;
-                        } else {
-                            return <AIChatMessage key={m.id} text={m.content}></AIChatMessage>;
-                        }
-                    })
-                    : null}
+  return (
+    <div className="flex  h-screen">
+      <div className=" w-full p-4   max-w-xl flex flex-col stretch   overflow-y-auto divide-y-2 divide-dashed divide-slate-700">
+        {messages.length > 0
+          ? messages.map((m, i: number) => {
+              if (m.role === "user") {
+                return (
+                  <UserChatMessage key={i} text={m.content}></UserChatMessage>
+                );
+              } else {
+                return <AIChatMessage key={i} text={m.content}></AIChatMessage>;
+              }
+            })
+          : null}
 
-                <form onSubmit={handleFormSubmit}>
-                    <input
-                        className="fixed w-full max-w-md bottom-0 border bg-white border-gray-300 rounded mb-8 shadow-xl p-2"
-                        ref={inputRef}
-                        type="text"
-                        placeholder="Say something..."
-                    // onChange={handleInputChange}
-                    />
-                </form>
-            </div>
-            <Viewer />
-        </div>
-    );
+        <form onSubmit={handleFormSubmit}>
+          <input
+            className="fixed w-full max-w-md bottom-0 border bg-white border-gray-300 rounded mb-8 shadow-xl p-2"
+            ref={inputRef}
+            type="text"
+            placeholder="Say something..."
+            // onChange={handleInputChange}
+          />
+        </form>
+      </div>
+      <Viewer />
+    </div>
+  );
 }
